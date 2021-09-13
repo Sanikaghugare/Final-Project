a90fca5ac0300d4dbd9e61c510df9d380444c34c
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-09-13 19:10:03 +0530

    Add files via upload

---
+++ Gmail.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package billingmanagementsystem;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author hp
+ */
+public class Gmail {
+    public static void send(String to, String subject, String msg) {
+
+        final String user = "madalapurevaishnavi@gmail.com";
+//change accordingly  
+        final String pass = "9822009608";
+
+//1st step) Get the session object	
+        Properties props = new Properties();
+
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        // below mentioned mail.smtp.port is optional 
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+//2nd step)compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(msg);
+
+            //3rd step)send message
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+ }
+    public static void main(String[] args) {
+        
+    
+    String to="madalapurevaishnavi@gmail.com";
+		String subject="recipt";
+		String msg="thnaks for visiting us";
+		
+		Gmail.send(to, subject, msg);
+		System.out.println("message has been sent successfully");
+		
+	
+}
+
+}
